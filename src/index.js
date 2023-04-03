077c2582b8bef4db8142cec6f0b7dcf161c34c6b
Abdurahim Miftah <abdugreat3@gmail.com>
2023-04-03 17:37:34 +0300

    Add to-do-list project

---
+++ src/index.js
@@ -0,0 +1,61 @@
+import './style.css';
+import showTasks from './modules/showTasks.js';
+import { readLocalStorage, setLocalStorage } from './modules/localStorage.js';
+import addTask from './modules/addTask.js';
+import removeTask from './modules/removeTask.js';
+import updateIndex from './modules/updateIndex.js';
+import { editTask, updateTask } from './modules/editTask.js';
+import markCompleted from './modules/markCompleted.js';
+import deleteCompleted from './modules/deleteAllCompleted.js';
+
+const addInput = document.getElementById('addInput');
+const removeBtn = document.querySelector('.removeBtn');
+const toDoList = () => readLocalStorage() || [];
+showTasks(toDoList());
+// Add task
+addInput.addEventListener('keyup', ({ key }) => {
+  if (key === 'Enter') {
+    addTask(toDoList(), addInput.value);
+    showTasks(toDoList());
+    addInput.value = '';
+  }
+});
+// Edit task
+const param = (currentList, currentIndex, li) => {
+  const obj = editTask(li);
+  const description = obj.descriptionEle;
+  const { deleteIcon } = obj;
+
+  // Delete Task
+  deleteIcon.addEventListener('click', () => {
+    removeTask(currentList, currentIndex);
+    showTasks(toDoList());
+    const data = updateIndex(toDoList());
+    setLocalStorage(data);
+  });
+
+  // Rename Task
+  description.addEventListener('keyup', (event) => {
+    if (event.keyCode === 13) {
+      event.preventDefault();
+      const text = description.innerText;
+      const updatedTasks = updateTask(currentList, currentIndex, text);
+      setLocalStorage(updatedTasks);
+      showTasks(toDoList());
+    }
+  });
+};
+// update status of completed tasks
+const checkCompleted = (currentList, index) => {
+  const updatedList = markCompleted(index, currentList);
+  setLocalStorage(updatedList);
+  showTasks(toDoList());
+};
+removeBtn.addEventListener('click', () => {
+  const newList = deleteCompleted(toDoList());
+  const data = updateIndex(newList);
+  setLocalStorage(data);
+  showTasks(toDoList());
+});
+
+export { param, checkCompleted };
